ef0f5a12ee1ddbc876d40c29db624e0fa230596d
kimseulbi <ksb5166@gmail.com>
2019-04-01 22:53:09 +0900

    [FIX] 로직 변경
    roduce사용과 배열로 미리 변경

--- src/01-basic/basic.js
+++ src/01-basic/basic.js
@@ -1,11 +1,14 @@
-export default function sum(num) {
-  let result = 0;
-  if (typeof num !== 'number') {
-    return `숫자를 넣어주세요`;
-  } else {
-    for (let item of arguments) {
-      result += item;
+// default: 기본!
+// 한파일에 한번만 사용 할수 있음.
+// arguments:  유사 배열 객체(array-like object)이자 반복 가능한 객체(iterable object)로, 함수에 주어진 인수가 순서대로 저장되기 때문에 인덱스를 가지고 인수를 읽어오거나 for...of를 통해 순회할 수 있습니다
+export default function sum() {
+  const arr = Array.from(arguments);
+  const arrSum = arr.reduce((acc, item) => acc + item, 0);
+
+  for (const item of arr) {
+    if (typeof item !== 'number') {
+      return `숫자를 넣어주세요`;
     }
-    return result;
+    return arrSum;
   }
 }
