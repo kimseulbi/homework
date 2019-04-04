ea0ea532265542de19a889ef177007b3acd98d25
devJang <devjang2016@gmail.com>
2019-04-04 14:14:58 +0900

    Revert "Kimseulbi"

--- src/01-basic/basic.js
+++ src/01-basic/basic.js
@@ -1,11 +1,1 @@
-export default function sum() {
-  const arr = Array.from(arguments);
-  const arrSum = arr.reduce((acc, item) => acc + item, 0);
-
-  for (const item of arr) {
-    if (typeof item !== 'number') {
-      return `숫자를 넣어주세요`;
-    }
-    return arrSum;
-  }
-}
+export default function sum() {}
